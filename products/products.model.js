4d46c9d96113414308aca69d3e6a78732326ab80
vishal dogra <dogravishal1598@gmail.com>
2022-03-20 15:48:36 +0530

    seperaretd schemas and types for products and orders

---
+++ products/products.model.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    id: "redshoes",
+    description: "Red Shoe",
+    price: 42.12,
+  },
+  {
+    id: "bluejean",
+    description: "Blue Jeans",
+    price: 55.55,
+  },
+];
